2ec7164892534960c55bc2ded47842b1c14aa5da
Alex Su <alexanderytsu@gmail.com>
2023-03-03 10:47:24 +1100

    helper function to load proposals from DealIDs

--- actors/market/src/lib.rs
+++ actors/market/src/lib.rs
@@ -472,18 +472,19 @@ impl Actor {
         let miner_addr = rt.message().caller();
         let curr_epoch = rt.curr_epoch();
 
+        // TODO: remove
         let st: State = rt.state()?;
         let proposals = st.get_proposal_array(rt.store())?;
 
         let mut sectors_data = Vec::with_capacity(params.sectors.len());
         for sector in params.sectors.iter() {
+            let sector_proposals = deal_proposals(rt, &sector.deal_ids)?;
             let sector_size = sector
                 .sector_type
                 .sector_size()
                 .map_err(|e| actor_error!(illegal_argument, "sector size unknown: {}", e))?;
             validate_and_return_deal_space(
-                &proposals,
-                &sector.deal_ids,
+                &sector_proposals,
                 &miner_addr,
                 sector.sector_expiry,
                 curr_epoch,
@@ -502,6 +503,7 @@ impl Actor {
 
         Ok(VerifyDealsForActivationReturn { sectors: sectors_data })
     }
+
     /// Activate a set of deals, returning the combined deal space and extra info for verified deals.
     fn activate_deals(
         rt: &mut impl Runtime,
@@ -511,13 +513,10 @@ impl Actor {
         let miner_addr = rt.message().caller();
         let curr_epoch = rt.curr_epoch();
 
-        let st: State = rt.state()?;
-        let proposals = st.get_proposal_array(rt.store())?;
-
+        let deal_proposals = deal_proposals(rt, &params.deal_ids)?;
         let deal_spaces = {
             validate_and_return_deal_space(
-                &proposals,
-                &params.deal_ids,
+                &deal_proposals,
                 &miner_addr,
                 params.sector_expiry,
                 curr_epoch,
@@ -998,6 +997,35 @@ impl Actor {
     }
 }
 
+fn deal_proposals(
+    rt: &impl Runtime,
+    deal_ids: &[DealID],
+) -> Result<Vec<(DealID, DealProposal)>, ActorError> {
+    let state: State = rt.state()?;
+    let proposals = state.get_proposal_array(rt.store())?;
+
+    let sector_proposals = {
+        let mut sector_proposals = Vec::new();
+        let mut seen_deal_ids = BTreeSet::new();
+        for deal_id in deal_ids {
+            if !seen_deal_ids.insert(deal_id) {
+                return Err(actor_error!(
+                    illegal_argument,
+                    "duplicate deal ID {} in sector",
+                    deal_id
+                ));
+            }
+            let proposal = proposals
+                .get(*deal_id)
+                .context_code(ExitCode::USR_ILLEGAL_STATE, "failed to load deal")?
+                .ok_or_else(|| actor_error!(not_found, "no such deal {}", deal_id))?;
+            sector_proposals.push((*deal_id, proposal.clone()));
+        }
+        sector_proposals
+    };
+    Ok(sector_proposals)
+}
+
 fn compute_data_commitment<BS: Blockstore>(
     rt: &impl Runtime,
     proposals: &DealArray<BS>,
@@ -1024,32 +1052,17 @@ fn compute_data_commitment<BS: Blockstore>(
     })
 }
 
-pub fn validate_and_return_deal_space<BS: Blockstore>(
-    proposals: &DealArray<BS>,
-    deal_ids: &[DealID],
+pub fn validate_and_return_deal_space(
+    sector_proposals: &[(DealID, DealProposal)],
     miner_addr: &Address,
     sector_expiry: ChainEpoch,
     sector_activation: ChainEpoch,
     sector_size: Option<SectorSize>,
 ) -> Result<DealSpaces, ActorError> {
-    let mut seen_deal_ids = BTreeSet::new();
     let mut deal_space = BigInt::zero();
     let mut verified_deal_space = BigInt::zero();
 
-    for deal_id in deal_ids {
-        if !seen_deal_ids.insert(deal_id) {
-            return Err(actor_error!(
-                illegal_argument,
-                "deal id {} present multiple times",
-                deal_id
-            ));
-        }
-
-        let proposal = proposals
-            .get(*deal_id)
-            .context_code(ExitCode::USR_ILLEGAL_STATE, "failed to load deal")?
-            .ok_or_else(|| actor_error!(not_found, "no such deal {}", deal_id))?;
-
+    for (deal_id, proposal) in sector_proposals {
         validate_deal_can_activate(proposal, miner_addr, sector_expiry, sector_activation)
             .with_context(|| format!("cannot activate deal {}", deal_id))?;
 
